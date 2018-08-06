c04e60b559428a16bc14f9642e47dbeb24e9e47c
Tianz <ssstz@outlook.com>
2018-08-06 09:55:49 +0800

    Update train_segmentation.py

--- train_segmentation.py
+++ train_segmentation.py
@@ -2,7 +2,6 @@ from models import unet, segnet
 from utils import segdata_generator
 from keras.optimizers import Adam, SGD
 from keras import backend as K
-from utils.schedules import onetenth_20_30
 from keras.callbacks import ModelCheckpoint
 import os
 import argparse
@@ -52,16 +51,16 @@ def save_history(history, result_dir, prefix):
 
 
 def main():
-    nClasses = 2
+    nClasses = args.nClasses
     train_batch_size = 16
     val_batch_size = 16
-    init_lr = 0.01
     epochs = 50
     img_height = 256
     img_width = 256
-    root_path = '/home/deep/datasets/'
-    train_file = './data/seg_train.txt'
-    val_file = './data/seg_test.txt'
+    root_path = '../../datasets/segmentation/'
+    mode = 'seg' if nClasses == 2 else 'parse'
+    train_file = './data/{}_train.txt'.format(mode)
+    val_file = './data/{}_test.txt'.format(mode)
     if args.model == 'unet':
         model = unet.Unet(nClasses, input_height=img_height, input_width=img_width)
     elif args.model == 'segnet':
@@ -69,37 +68,35 @@ def main():
     else:
         raise ValueError('Does not support {}, only supports unet and segnet now'.format(args.model))
 
-    sgd = SGD(lr=init_lr, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
                   optimizer=Adam(lr=1e-4),
                   metrics=['accuracy'])
     model.summary()
 
-
     train = segdata_generator.generator(root_path, train_file, train_batch_size, nClasses, img_height, img_width)
 
     val = segdata_generator.generator(root_path, val_file, val_batch_size, nClasses, img_height, img_width, train=False)
 
-    checkpoint = ModelCheckpoint('./results/{}_weights.h5'.format(args.model),
-                                 monitor='val_acc', save_best_only=True, save_weights_only=True, verbose=1)
+    if not os.path.exists('./results/'):
+        os.mkdir('./results')
+    save_file = './weights/{}_seg_weights.h5'.format(args.model) if nClasses == 2 \
+        else './weights/{}_parse_weights.h5'.format(args.model)
+    checkpoint = ModelCheckpoint(save_file, monitor='val_acc', save_best_only=True, save_weights_only=True, verbose=1)
     history = model.fit_generator(train,
                         steps_per_epoch=12706 // train_batch_size,
                         validation_data=val,
                         validation_steps=5000 // val_batch_size,
                         epochs=epochs,
                         callbacks=[checkpoint],
-                        # callbacks=[onetenth_20_30(init_lr), checkpoint]
                                   )
     plot_history(history, './results/', args.model)
     save_history(history, './results/', args.model)
-    if not os.path.exists('./results/'):
-        os.mkdir('./results')
-    model.save_weights('./results/{}_weights.h5'.format(args.model))
 
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser(description='command for training segmentation models with keras')
     parse.add_argument('--model', type=str, default='segnet', help='support unet, segnet')
+    parse.add_argument('--nClasses', type=int, default=2)
     args = parse.parse_args()
 
     main()
